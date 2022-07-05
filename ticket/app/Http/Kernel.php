f3f31441531ef0b2f58ecf32b9167d20e9df92db
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2022-07-05 10:41:55 +0700

    :closed_lock_with_key: Register new middleware to Kernel
    Register new middleware to Kernel

--- ticket/app/Http/Kernel.php
+++ ticket/app/Http/Kernel.php
@@ -52,5 +52,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
+        'only.json' => \App\Http\Middleware\OnlyAcceptJsonMiddleware::class,
     ];
 }
