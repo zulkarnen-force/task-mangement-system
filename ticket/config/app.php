881810cd0ed4f4b643e9ee34f3032dfc4813599c
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2022-07-05 15:27:57 +0700

    add JWT configuration

--- ticket/config/app.php
+++ ticket/config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Baum\Providers\BaumServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         //
 
         /*
@@ -224,6 +225,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
